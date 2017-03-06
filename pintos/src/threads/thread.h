736bffd8f17e624d54a2dbe79e8a0f4b8bcaa189
Andrew Massey <amassey7@ksu.edu>
2017-03-05 19:58:56 -0600

    Included exit_status for referencing

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -96,6 +96,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    int exit_status;                    /* Holds the exit status of thread */
     struct thread *parent;              /* marks the Parent thread */
     struct list_elem *kiddo;            /* marks the child thread */
     struct list *kids;                  /* marks all the rugrats */
