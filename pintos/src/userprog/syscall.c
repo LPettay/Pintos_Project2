841c0912377858e632b756174bd8bb810f948b1e
Andrew Massey <amassey7@ksu.edu>
2017-03-13 13:14:13 -0500

    fixes 1.1.9.5

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -313,7 +313,7 @@ static void get_args(struct intr_frame *f, int* args, int n)
   }
 }
 
-static struct process_file* get_process_file(int file_descriptor) 
+static struct process_file * get_process_file(int file_descriptor) 
 {
   /* Acquire file system lock                               */
   lock_acquire(&file_sys_lock);
