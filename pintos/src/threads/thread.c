b4483b3c8cb7d4d79aa516cc679911d7de1c1af5
Andrew Massey <amassey7@ksu.edu>
2017-03-13 22:22:51 -0500

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -50,6 +50,7 @@ struct kernel_thread_frame
 static long long idle_ticks;    /* # of timer ticks spent idle. */
 static long long kernel_ticks;  /* # of timer ticks in kernel threads. */
 static long long user_ticks;    /* # of timer ticks in user programs. */
+static struct thread *thread_get(int pid);
 
 /* Scheduling. */
 #define TIME_SLICE 4            /* # of timer ticks to give each thread. */
